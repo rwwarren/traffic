48d8cf21fd6be420a965b0e770ba74f7e5fc5054
ericp <ericproulx@porch.com>
2017-04-24 16:18:39 -0700

    updated showbox dto

---
+++ src/main/java/DTOs/ShowBoxEventDTO.java
@@ -0,0 +1,58 @@
+package DTOs;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ShowBoxEventDTO {
+    private final LocalDateTime eventTime;
+    private final String venue;
+    private final String band;
+
+    public ShowBoxEventDTO(@JsonProperty("eventTime") LocalDateTime eventTime,
+                           @JsonProperty("venue") String venue,
+                           @JsonProperty("band") String band) {
+        this.eventTime = eventTime;
+        this.venue = venue;
+        this.band = band;
+    }
+
+    public LocalDateTime getEventTime() {
+        return eventTime;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public String getBand() {
+        return band;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ShowBoxEventDTO that = (ShowBoxEventDTO) o;
+        return Objects.equals(eventTime, that.eventTime) &&
+                Objects.equals(venue, that.venue) &&
+                Objects.equals(band, that.band);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(eventTime, venue, band);
+    }
+
+    @Override
+    public String toString() {
+        return "ShowBoxEventDTO{" +
+                "eventTime=" + eventTime +
+                ", venue='" + venue + '\'' +
+                ", band='" + band + '\'' +
+                '}';
+    }
+}
--- src/main/java/resources/What.java
+++ src/main/java/resources/What.java
@@ -1,24 +1,32 @@
 package resources;
 
+import DTOs.ShowBoxEventDTO;
 import DTOs.WSCCEventDTO;
+import org.joda.time.DateTime;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Path("/what")
+@Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class What {
 
@@ -26,8 +34,8 @@ public class What {
     }
 
     @GET
-    @Path("/events")
-    public Set<WSCCEventDTO> whatEvent() throws Exception {
+    @Path("/wscc")
+    public Set<WSCCEventDTO> getWsccEvents() throws Exception {
 //        http://www.wscc.com/upcoming-events
 //        http://www.wsdot.com/traffic/trafficalerts/
 //        http://www.seattle.gov/event-calendar
@@ -44,29 +52,8 @@ public class What {
         if (first.isPresent()) {
             Elements li = first.get().select("li");
             Set<WSCCEventDTO> wsccEvents = li.stream()
-                    .map(element -> {
-                                Date startDate = null;
-                                Date endDate = null;
-                                try {
-                                    String dateRangeRaw = element.select("div.views-field-field-event-start-date-2").select("div.field-content").text();
-                                    if (dateRangeRaw.contains("-")) {
-                                        String[] split = dateRangeRaw.split("-");
-                                        startDate = dateFormatter(split[0]);
-                                        endDate = dateFormatter(split[0]);
-                                    } else {
-                                        startDate = dateFormatter(dateRangeRaw);
-                                    }
-                                } catch (ParseException e) {
-                                    System.out.println(e);
-                                }
-                                String est = element.select("div.views-field-field-event-attendance").select("span.field-content").text().replaceAll(",", "");
-                                return new WSCCEventDTO(
-                                        element.select("div.views-field-field-event-description").select("div.field-content").text(),
-                                        startDate,
-                                        endDate,
-                                        est.isEmpty() ? 0 : Integer.parseInt(est));
-                            }
-                    ).collect(Collectors.toSet());
+                    .map(elementWSCCEventDTOFunction)
+                    .collect(Collectors.toSet());
             return wsccEvents;
         } else {
             return Collections.emptySet();
@@ -77,4 +64,50 @@ public class What {
         SimpleDateFormat rangeFormatter = new SimpleDateFormat("MMM d");
         return rangeFormatter.parse(rawInput.replaceAll("(?<=\\d)(st|nd|rd|th)", "").trim());
     }
+
+    private Function<Element, WSCCEventDTO> elementWSCCEventDTOFunction = element -> {
+        Date startDate;
+        Date endDate = null;
+        try {
+            String dateRangeRaw = element.select("div.views-field-field-event-start-date-2").select("div.field-content").text();
+            if (dateRangeRaw.contains("-")) {
+                String[] split = dateRangeRaw.split("-");
+                startDate = dateFormatter(split[0]);
+                endDate = dateFormatter(split[0]);
+            } else {
+                startDate = dateFormatter(dateRangeRaw);
+            }
+        } catch (ParseException e) {
+            throw new WebApplicationException(e.getCause());
+        }
+        String est = element.select("div.views-field-field-event-attendance").select("span.field-content").text().replaceAll(",", "");
+        return new WSCCEventDTO(
+                element.select("div.views-field-field-event-description").select("div.field-content").text(),
+                startDate,
+                endDate,
+                est.isEmpty() ? 0 : Integer.parseInt(est));
+    };
+
+    @GET
+    @Path("/showbox")
+    public Set<ShowBoxEventDTO> getShowBoxEvents() throws Exception {
+        Document document = Jsoup.connect("http://www.showboxpresents.com/events/all").get();
+        Elements eventsList = document.getElementById("eventsList").getElementsByClass("entry");
+        Set<ShowBoxEventDTO> showboxEvents = eventsList.stream()
+                .filter(element -> element.attr("data-state").equals("WA"))
+                .map(element -> {
+                    Element infoElement = element.getElementsByClass("info").first();
+                    String bandName = infoElement.getElementsByClass("title").first().text();
+                    Element dateTime = infoElement.getElementsByClass("date-time-container").first();
+                    String date = dateTime.select("span.date").first().text();
+                    String time = dateTime.select("span.time").first().text().replaceAll("Show", "").trim();
+                    String venue = dateTime.select("span.venue").first().text();
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("E, MMM d, y h:m a");
+                    LocalDateTime formattedDate = LocalDateTime.parse(date + " " + time, formatter);
+                    return new ShowBoxEventDTO(formattedDate, venue, bandName);
+                })
+                .collect(Collectors.toSet());
+        return showboxEvents;
+    }
+
 }
